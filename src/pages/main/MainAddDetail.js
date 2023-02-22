bbadde05bdd8c587b148be0c3411c8c6d683df50
choj96 <choj9603@gmail.com>
2023-02-22 12:01:44 +0900

    소비합계 출력

--- src/pages/main/MainAddDetail.js
+++ src/pages/main/MainAddDetail.js
@@ -1,27 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
 import { FaCapsules } from "react-icons/fa";
 import { FaThumbtack } from "react-icons/fa";
 import tw from "tailwind-styled-components";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { BiCategory } from "react-icons/bi";
 const MainAddD = () => {
+  const user = useSelector((state) => state.user);
   const navigate = useNavigate();
-  let initVal = {
-    edtitle: "",
-    cateSeq: "",
-    edDate: "",
-    edAmont: "",
+  const [won, setWon] = useState("");
+  const [history, setHistory] = useState("");
+  const [day, setDay] = useState("");
+  const [category, setCategory] = useState([]);
+  const [selected, setSelected] = useState("");
+  const [top, setTop] = useState([]);
+  const expenditure = (e) => {
+    let body = {
+      edtitle: history,
+      cateSeq: selected,
+      edDate: day,
+      edAmont: won,
+    };
+    axios
+      .put(`http://192.168.0.151:9898/expenses/insert/${user.miSeq}`, body)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
-  const [val, setVal] = useState(initVal);
-
-  const handleChange = (e) => {
-    // console.log(e.target); // tag = {name:"userid", value:"123"}
-    // console.log(e.target.name); // tag name="userid"
-    // console.log(e.target.value);// tag value
-    const { name, value } = e.target;
-    setVal({ ...val, [name]: value });
+  const cate = async () => {
+    try {
+      const res = await axios.get(
+        "http://192.168.0.151:9898/expenses/categoryList"
+      );
+      setCategory(res.data);
+      // console.log(res.data.map((item) => item.cateSeq));
+      const res2 = await axios.get(
+        `http://192.168.0.151:9898/expenses/NmonthTop3/${user.miSeq}`
+      );
+      setTop(res2.data);
+      console.log(res2.data.map((item) => item.edSeq));
+    } catch (err) {
+      console.log(err);
+    }
   };
 
+  useEffect(() => {
+    cate();
+  }, []);
+
+  function priceToString(price) {
+    if (price === undefined || price === null) return;
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
   return (
     <article>
       <div>
@@ -29,7 +64,7 @@ const MainAddD = () => {
           <button onClick={() => navigate(-1)}>
             <MdOutlineKeyboardArrowLeft className="text-sub text-5xl font-bold" />
           </button>
-          
+
           <h1 className="text-xl font-bold text-main">내역추가</h1>
         </Header>
         <div>
@@ -38,45 +73,69 @@ const MainAddD = () => {
               <input
                 placeholder="- 소비금액 원"
                 className="hover:outline-white ml-3 outline-white"
-                onChange={handleChange}
+                onChange={(e) => {
+                  setWon(e.target.value);
+                }}
               />
             </MainBt>
             <MainBt className="flex text-xl">
               <FaCapsules className="text-main text-3xl font-bold " />
+              <select
+                onChange={(e) => {
+                  setSelected(e.target.value);
+                }}
+              >
+                {category.map((item, index) => (
+                  <option key={index} value={item.cateSeq}>
+                    {item.cateName}
+                  </option>
+                ))}
+              </select>
               <input
                 placeholder="내역"
                 className="ml-3 hover:outline-white outline-white"
-                onChange={handleChange}
+                onChange={(e) => {
+                  setHistory(e.target.value);
+                }}
               />
             </MainBt>
             <MainBt className="flex text-xl ">
               <input
                 type="date"
                 className="ml-3 hover:outline-white outline-white"
-                onChange={handleChange}
+                onChange={(e) => {
+                  setDay(e.target.value);
+                }}
               />
             </MainBt>
           </div>
         </div>
 
         <h1 className="text-xl  text-main pl-8 ">최근 소비</h1>
+        {top.map((item, index) => (
+          <div className="  px-8 pt-5" key={index}>
+            <MainBt className="w-full">
+              <div className="flex items-center mb-3">
+                <FaThumbtack className=" text-main text-base font-bold " />
+                <span className="text-base font-bol text-main">
+                  {item.edTitle}
+                </span>
+              </div>
+              <div className="flex justify-between items-center">
+                <span className="text-xs  text-sub">{item.edDate}</span>
+                <span className="text-base text-sub pl-20">
+                  {priceToString(item.edAmount)} 원
+                </span>
+              </div>
+            </MainBt>
+          </div>
+        ))}
 
-        <div className="  px-8 pt-5">
-          <MainBt className="w-full ">
-            <div className="flex items-center mb-3">
-              <FaThumbtack className=" text-main text-base font-bold " />
-              <span className="text-base font-bol text-main">
-                고정지출입니다
-              </span>
-            </div>
-            <div className="flex justify-between items-center">
-              <span className="text-xs  text-sub">2월 14일</span>
-              <span className="text-base text-sub pl-20">50,000 원</span>
-            </div>
-          </MainBt>
-        </div>
         <div className="  p-8 ">
-          <MainBt className="w-5/6 absolute bottom-36 inline-block bg-main  text-white">
+          <MainBt
+            className="w-5/6 absolute bottom-36 inline-block bg-main  text-white"
+            onClick={() => expenditure()}
+          >
             저장하기
           </MainBt>
         </div>
